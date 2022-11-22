ec97aee11a522204455b12644b94816b7c12d218
anastasiya <aishkova@big3.ru>
2022-11-22 23:10:50 +0300

    Добавлено решение задачи 2

--- C# practic 2/Program.cs
+++ C# practic 2/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+Console.WriteLine("Введите 3 числа:");
+int a = Convert.ToInt32(Console.ReadLine());
+int b = Convert.ToInt32(Console.ReadLine());
+int c = Convert.ToInt32(Console.ReadLine());
+int max = a;
+if (b > max)
+{
+    max = b;
+}
+ if (c > max)
+ {
+    max = c;
+ }
+ Console.WriteLine("Наибольшее из введённых чисел -> " + max);
+ // Проверено на числах: 2,3,7 ; 44,5,78 ; 22,3,9. 
