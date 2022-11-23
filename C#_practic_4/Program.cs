b5a1f2173835b47f33bd921b8006a8526d9a597c
anastasiya <aishkova@big3.ru>
2022-11-23 13:44:59 +0300

    Добавили решение задачи 4

--- C#_practic_4/Program.cs
+++ C#_practic_4/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 8: Напишите программу, которая на вход принимает число (N), 
+// а на выходе показывает все чётные числа от 1 до N.
+Console.Clear();
+int i = 1;
+bool not = true;
+Console.WriteLine("Введите число:");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Чётные числа от 1 до " + n);
+while (i <= n)
+{
+     if (i % 2 != 1)
+     {
+        Console.Write(i + ", ");
+        not = false;
+    }
+        i++;
+}
+
+if (not)
+{
+    Console.WriteLine("Нет чётных чисел!");
+}
+// Проверено на 4,6,8
