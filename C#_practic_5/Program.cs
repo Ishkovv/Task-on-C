b748aaf0452270dfbe2c4f662d5b97afdaa6625c
anastasiya <aishkova@big3.ru>
2022-11-28 13:13:47 +0300

    Добавлено решение задачи 5

---
+++ C#_practic_5/Program.cs
@@ -0,0 +1,24 @@
+﻿// Напигите программу, которая принимает на вход трехзначное число 
+//и на выходе показывает вторую цифру этого числа
+int n = ReadInt("Введите трехзначное число: ");
+int amount = n.ToString().Length;
+{
+    Console.WriteLine(InCenter(n));
+}
+
+// Функция принимает сообщение для отображения в консоли, и выводит результат введенных данных пользователем. 
+int ReadInt(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+// Функция вывода цифры стоящей в середине трехзначногно числа.
+int InCenter(int a)
+{
+    
+    int result = ((a / 10) % 10);
+    return result;
+}
+
+// Проверено на 456, 678, 918
